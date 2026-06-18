9cf49c66f9f4934e1e092245291b299a5c07684c
rohannotfound-ctrl <rohansinghg371@gmail.com>
2026-06-18 21:44:38 +0530

    Time: 75 ms (49.18%) | Memory: 46.7 MB (71.36%) - LeetSync

---
+++ 459-repeated-substring-pattern/repeated-substring-pattern.java
@@ -0,0 +1,8 @@
+class Solution {
+    public boolean repeatedSubstringPattern(String s) {
+        String doubled = s + s;
+        String modified = doubled.substring(1, doubled.length() - 1);
+
+        return modified.contains(s);
+    }
+}
